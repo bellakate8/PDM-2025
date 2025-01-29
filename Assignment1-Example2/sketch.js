e8a88e8065c11200db60bacdf6d36b5f47818154
bellakate8 <ipost7780@gmail.com>
2025-01-28 21:35:14 -0600

    Add files via upload
    Assignment1-Example2

---
+++ Assignment1-Example2/sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(400, 400);
+  noStroke();
+}
+
+function draw() {
+  background(255,255,255);
+
+ 
+ fill(0,255,0,100); //green
+  circle(250,300,200);
+  
+
+fill(255,0,0,100); //red
+  circle(190,190,200);
+  
+ fill(0,0,255,100); //blue
+  circle(130,300,200);
+
+}
